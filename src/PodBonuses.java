ab4227c7b2d486137999e35dcd1f7b012d3ffa5a
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-16 16:50:54 -0500

    Created PodBonuses java file

---
+++ src/PodBonuses.java
@@ -0,0 +1,18 @@
+public class PodBonuses {
+
+    public static void main(String[] args) {
+        //random easy stuff that I made up:
+        String buffalo = "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo ";
+        String bison = "Buffalo bison, that other Buffalo bison bully, also bully Buffalo bison.";
+        String connectingSentence = "is a valid sentence and is translated to";
+
+        System.out.print(buffalo);
+        System.out.println(connectingSentence);
+        System.out.println(bison);
+
+
+
+    }
+
+
+}
