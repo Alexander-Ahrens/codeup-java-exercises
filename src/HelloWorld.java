a32779a9e13fc03c2212ea5a1ed11beaf8aeec7b
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-16 16:11:16 -0500

    completed lesson 2 for helloWorld exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,58 @@
 public class HelloWorld {
 
     public static void main (String[] args) {
+//        System.out.print("Hello ");
+//        System.out.print("World!");
+//        System.out.println("");
 //        System.out.println("Hello World!");
-        System.out.print("Hello ");
-        System.out.print("World!");
+//
+//        int myFavoriteNumber = 8;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Sometimes, you just gotta dance.";
+//        System.out.println(myString);
+//
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+//
+//        double myNumberToo = 3.14;
+//        System.out.println(myNumberToo);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int z = 5;
+//        System.out.println(++z);
+//        System.out.println(z);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println();
+
+        int r = 4;
+//        r = r + 5;
+        r += 5;
+        System.out.println(r);
+
+        int s = 3;
+        int t = 4;
+//        t = t * s;
+        t *= s;
+        System.out.println(s);
+        System.out.println(t);
+
+        int a = 10;
+        int b = 2;
+//        a = a / b;
+//        b = b - a;
+        a /= b;
+        b -= a;
+        System.out.println(a);
+        System.out.println(b);
+
+
     }
 
 }
