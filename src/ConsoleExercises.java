e098d02a14a5829c3b18725b50bb5b9dd034f6ea
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-17 12:30:01 -0500

    Created and finished ConsoleExercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //============ QUESTION 1 ==========//
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately: %f.\n", pi);
+//        System.out.println("|");
+
+        //============ QUESTION 2 ==========//
+//        System.out.println("Enter whatever number you feel like.");
+//        int number = scan.nextInt();
+//        System.out.println("You entered the number " + number + ".");
+
+        //============ QUESTION 3 ==========//
+//        System.out.println("Enter three random words.");
+
+//        String word1 = scan.next();
+//        String word2 = scan.next();
+//        String word3 = scan.next();
+//        System.out.println(word1);
+//        System.out.println(word2);
+//        System.out.println(word3);
+
+        //============= QUESTION 4 ==========//
+//        System.out.println("Tell me your life story.");
+//        String sentence = scan.nextLine();
+//        System.out.println(sentence);
+
+        //============= QUESTION 5 ==========//
+//        System.out.println("Calculate the perimeter of the classroom.");
+//        System.out.println("What is the width of the classroom in feet?");
+//        int width = scan.nextInt();
+//
+//        System.out.println("What is the length of the classroom in feet?");
+//        int length = scan.nextInt();
+//
+//        int p = ((length * 2) + (width * 2));
+//        System.out.println("The perimeter of the room is " + p + "!");
+
+        //============= QUESTION 5 REDONE ============//
+//        System.out.println("Give me the length: ");
+//        int length = Integer.parseInt(scan.nextLine());
+//        System.out.println("Give me the width: ");
+//        int width = Integer.parseInt(scan.nextLine());
+//
+//        int area = length * width;
+//        int perimeter = (length * 2) + (width * 2);
+//
+//        System.out.println("Perimeter is: " + perimeter);
+//        System.out.println("Area is: " + area);
+
+
+
+
+
+    }
+
+}
