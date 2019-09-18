f36c85a7ba79af24a79387b4d82f4af03712bc4e
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-18 13:46:11 -0500

    Finished pig latin bonus

--- src/PigLatinBonus.java
+++ src/PigLatinBonus.java
@@ -3,20 +3,28 @@ import java.util.Scanner;
 
 //  String cat = "cat"; // "atcay
 
+//String word1 = "cat";
+//        String pigLatinWord = word1
+//        .trim()
+//        .toLowerCase()
+//        .substring(1)
+//        .concat(word1.substring(0, 1).toLowerCase())
+//        .concat("ay");
+//        System.out.println(pigLatinWord);
+
 public class PigLatinBonus {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String word = "";
-
+//        String word = "";
         System.out.println("Type a word to convert it to pig latin.");
         String input = scan.nextLine();
-//        System.out.println(input);
-        if (word.startsWith("a, e, i, o, u")) {
-            word.replace("")
-
-        }
+        String word = input.trim()
+                            .toLowerCase()
+                            .substring(1)
+                            .concat(input.substring(0, 1).toLowerCase())
+                            .concat("ay");
+        System.out.println(word);
 
     }
-
 }
 
