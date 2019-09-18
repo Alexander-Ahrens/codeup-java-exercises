2d0ff379174238ed7898a2db92003cd48d096641
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-18 12:20:25 -0500

    added PigLatinBonus and modified StringExercise

---
+++ src/PigLatinBonus.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+//
+//
+////  String cat = "cat"; // "atcay
+//
+//public class PigLatinBonus {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        String word = "";
+//
+//        System.out.println("Type a word to convert it to pig latin.");
+//        String input = scan.nextLine();
+////        System.out.println(input);
+////        if (word indexOf(0) == "a, e, i o, u") {
+//
+//        }
+//
+//    }
+//}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -4,8 +4,23 @@ public class StringExercise {
                 "We don't need no thought control\n" +
                 "Check \"this\" out!, \"s inside of \"s! \n" +
                 "In windows, the main drive is usually C:\\ \n" +
-                "I can do backslashes \\, double backslash \\\\, \n " +
+                "I can do backslashes \\, double backslash \\\\, \n" +
                 "and the amazing triple backslash \\\\\\!";
         System.out.println(message);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        String message1 = "We don't need no education";
+        String message2 = "We don't need no thought control";
+        String message3 = "Check \"this\" out!, \"s inside of \"s!";
+        String message4 = "In windows, the main drive is usually C:\\";
+        String message5 = "I can do backslashes \\, double backslash \\\\,";
+        String message6 = "and the amazing triple backslash \\\\\\!";
+        System.out.println(message1);
+        System.out.println(message2);
+        System.out.println(message3);
+        System.out.println(message4);
+        System.out.println(message5);
+        System.out.println(message6);
     }
 }
