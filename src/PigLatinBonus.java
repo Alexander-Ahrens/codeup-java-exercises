f9afa5a12dba1b021f6761771d3d6aee6930e797
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-18 12:30:45 -0500

    working on PigLatinBonus

--- src/PigLatinBonus.java
+++ src/PigLatinBonus.java
@@ -1,19 +1,22 @@
-//import java.util.Scanner;
-//
-//
-////  String cat = "cat"; // "atcay
-//
-//public class PigLatinBonus {
-//    public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        String word = "";
-//
-//        System.out.println("Type a word to convert it to pig latin.");
-//        String input = scan.nextLine();
-////        System.out.println(input);
-////        if (word indexOf(0) == "a, e, i o, u") {
-//
-//        }
-//
-//    }
-//}
+import java.util.Scanner;
+
+
+//  String cat = "cat"; // "atcay
+
+public class PigLatinBonus {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String word = "";
+
+        System.out.println("Type a word to convert it to pig latin.");
+        String input = scan.nextLine();
+//        System.out.println(input);
+        if (word.startsWith("a, e, i, o, u")) {
+            word.replace("")
+
+        }
+
+    }
+
+}
+
