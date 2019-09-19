0107450ded596ee4ad84d490c020503650a23632
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-19 11:58:22 -0500

    Completed up to nuber 2 for MethodsExercise

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,12 +1,21 @@
+import java.math.*;
+import java.util.*;
+import java.lang.Integer;
+
 public class MethodsExercise {
+
     public static void main(String[] args) {
-        System.out.println(add(2, 8));
-        System.out.println(subtract(93, 18));
-        System.out.println(multiply(16, 4));
-        System.out.println(divide(32, 8));
+//        System.out.println(add(2, 8));
+//        System.out.println(subtract(93, 18));
+//        System.out.println(multiply(16, 4));
+//        System.out.println(divide(32, 4));
+//        System.out.println(modulo(8, 2));
+        count(5);
+        System.out.println(recursiveProduct(3, 22));
 
     }
 
+    //============================================================ EXERCISE ONE ============================================================//
     //========== ADDS TWO NUMBERS ==========//
     public static int add(int num1, int num2) {
         return num1 + num2;
@@ -26,4 +35,60 @@ public class MethodsExercise {
     public static int divide(int numero1, int numero2) {
         return numero1 / numero2;
     }
+
+    //========== FIND THE MODULO ==========//
+    public static int modulo(int potMod1, int potMod2) {
+        return potMod1 % potMod2;
+    }
+
+    //========== MULTIPLY USING RECURSION WITHOUT * OPERATOR ==========//
+    public static int recursiveProduct(int mult1, int mult2) {
+        if (mult1 < mult2) {
+            return recursiveProduct(mult2, mult1);
+        } else if (mult2 != 0) {
+            return (mult1 + recursiveProduct(mult1, mult2 - 1));
+        } else {
+            return 0;
+        }
+    }
+
+    //========== RECURSION EXAMPLE ==========//
+    public static void count(int n) {
+        if (n <= 0) {
+            System.out.println("All done!");
+            return;
+        }
+        System.out.println(n);
+        count(n - 1);
+        //count(5); is called in the main method.
+    }
+
+    //============================================================  EXERCISE 2 ============================================================//
+    // Prompt the user to enter an integer from 1 to 10.
+    public static int oneToTen(int thing) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number between 1 and 10.");
+        Integer input = scan.nextInt();
+        if (input >= 1 && input <= 10) {
+            System.out.println("Hey person! That ain't no number between one and ten! Try again lil' buddy!");
+        } else {
+            System.out.println();
+        }
+        return thing;
+    }
+
+    // Display the factorial of the number entered by the user.
+    // Ask if the user wants to continue.
+    // Use a for loop to calculate the factorial.
+    // Assume that the user will enter an integer, but verify it’s between 1 and 10.
+    // Use the long type to store the factorial.
+    // Continue only if the user agrees to.
+    // A factorial is a number multiplied by each of the numbers before it.
+    // Factorials are denoted by the exclamation point (n!).
+
+    //============================================================  EXERCISE 3 ============================================================//
+
+
+    //============================================================  EXERCISE 4 ============================================================//
+
 }
