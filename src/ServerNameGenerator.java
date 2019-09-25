7e9b8fc309cedbb0546388152a954e22290110da
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-25 14:59:22 -0500

    Created ServerNameGenerator and add main method and array of nouns and adjectives

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,18 @@
 public class ServerNameGenerator {
+
+    String[] adjectives = {"Ghostly", "Sickened", "Rockin", "Dashing", "Sinister", "Scary", "Happy", "Missing", "Glaring", "Programming"};
+    String[] nouns = {"Noun", "System", "Cloud", "Panda", "Macho Man Randy Savage", "Napkin", "Program", "Garbage", "Otter", "Leftovers"};
+
+
+    public void randomAdjectiveAndNoun() {
+        for (int i = 0; i <= 9; i++) {
+            double adjective = (int) Math.floor(Math.random() * 100);
+            double noun = (int) Math.floor(Math.random() * 100);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        return randomAdjectiveAndNoun;
+    }
 }
