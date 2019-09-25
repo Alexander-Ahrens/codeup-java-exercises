5efd60878bddac58432a37760193841060ee1537
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-25 11:31:07 -0500

    finished main exercises for person.java and input.java

--- src/Person.java
+++ src/Person.java
@@ -1,43 +1,53 @@
 //import java.util.Scanner;
 //
 public class Person {
+    // name property.
     private String name;
 
-            // returns the person's name
+
+    // Constructor ALWAYS has to be public
+    // can use setter to pass name.
+    public Person (String name) {  // <-----THIS SHIT IS A CONSTRUCTOR!!!
+//        setName(name);
+        // OR
+        this.name = name;
+    }
+
+            // Returns the person's name (does nothing but returns name).
+            // Also called a "getter".
             public String getName() {
                 return this.name;
             }
 
             // changes the name property to the passed value
+            // Needs a parameter
+            // Also called a "setter"
             public void setName(String name) {
+
+                // "this.name" refers to the private String name
+                // and " = name " is the name of the parameter.
                 this.name = name;
             }
 
             // prints a message to the console using the person's name
             public void sayHello() {
-                System.out.printf("Name: %s", name);
+                System.out.printf("Hello, %s", name);
+                // OR use the getter
+                System.out.println("Hello, " + this.getName());
             }
 
-        public Person(String person) {
-//            Scanner scan = new Scanner(System.in);
-//            System.out.println("What is your name?");
-            this.name = person;
-
-//
-
-        }
 
     public static void main(String[] args) {
+        Person alex = new Person("Alex"); // <---- THIS BITCH IS AN OBJECT!!!
 //        Person p1 = new Person("");
 //        Person pr = new Person("");
 //        System.out.println(s1.getName());
 //        System.out.println(s2.getName());
-        Person alex = new Person("Alex");
 
-            Person person1 = new Person("John");
-            Person person2 = new Person("John");
-            System.out.println(person1.getName().equals(person2.getName()));
-            System.out.println(person1 == person2);
+//            Person person1 = new Person("John");
+//            Person person2 = new Person("John");
+//            System.out.println(person1.getName().equals(person2.getName()));
+//            System.out.println(person1 == person2);
 
 //            Person person1 = new Person("John");
 //            Person person2 = person1;
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,45 +1,90 @@
 package util;
-
 import java.util.Scanner;
 
 public class Input {
+
+    //======================== THIS IS A SCANNER ========================//
+
+    // Creates scanner variable
     private Scanner scanner;
 
+    // Grabs input class with "this" and instantiates scanner...
+    // ...so that every new object that is created has a scanner.
     public Input() {
         this.scanner = new Scanner(System.in);
     }
 
-    public String getString() {
-        System.out.println("What is your name?");
+    // Returns whatever is input by the user.
+    public String getString() {          // used as "input.getString" to scan.
         return this.scanner.nextLine();
     }
-
-    boolean yesNo() {
-        if (this.scanner.nextLine().equalsIgnoreCase("y")) {
-            return true;
+    // Just a copy of the previous constructor
+    public String getString(String prompt) {
+        if (prompt.isEmpty()) {
+            System.out.println("Type something.");
         } else {
-            return false;
+            System.out.println(prompt);
+        }
+        return getString();
+    }
+
+    //====================================================================//
+
+    // If "y" or "yes" or any variation thereof is input, then
+    // method will return true. If anything else is input then method will
+    // return false.
+    public boolean yesNo() {
+//        if (this.scanner.nextLine().equalsIgnoreCase("y") ||
+//                this.scanner.nextLine().equalsIgnoreCase("yes")) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+        // OR (different solution)
+        System.out.println("Type y/n");
+        String answer = this.scanner.nextLine();
+        return (answer.equalsIgnoreCase("y") ||  // Ternary operator?
+                answer.equalsIgnoreCase("yes")); // Ternary operator?
+    }
+
+
+    //
+    public int getInt(int min, int max) {
+        System.out.println("Give me a number: ");
+        int num = Integer.parseInt(this.scanner.nextLine());  // this.scanner... is always a scanner.
+        if (num >= min && num <= max) {
+            return num;
+        } else {
+            System.out.println("Number is out of range. Try a different number.");
+            return getInt(min, max);  // How Recursion actually works.
         }
     }
 
-    int getInt(int min, int max) {
 
+    // Sometimes we have a range, sometimes we don't care
+    // That's why overloading is a thing. (Overloading = More than one constructor (method?) with the same name.)
+    public int getInt() {
+        System.out.println("Give me a number: ");
+        return Integer.parseInt(this.scanner.nextLine());
     }
 
-    int getInt() {
 
+    public double getDouble(double min, double max) {
+        System.out.println("Give me a number: ");
+        double num = Integer.parseInt(this.scanner.nextLine());
+        if (num >= min && num <= max) {
+            return num;
+        } else {
+            System.out.println("Number is out of range. Try a different number.");
+            return getDouble(min, max);
+        }
     }
 
-    double getDouble(double min, double max) {
 
+    public double getDouble() {
+        System.out.println("Give me a number: ");
+        return Double.parseDouble(this.scanner.nextLine());
     }
-
-    double getDouble() {
-
-    }
-
-    public inputTest() {
-        Input input  = new Input();
-    }
-
 }
+
+//==================== Bonus ====================//
